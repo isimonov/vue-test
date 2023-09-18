76b63d70552cc402e6b0ba82e15c71ae1207903d
Ivan Simonov <isimonov@sberbank-tele.com>
2023-09-18 23:05:22 +0300

    fix: login.js add login function

--- pages/login.js
+++ pages/login.js
@@ -2,6 +2,15 @@ import { ref } from 'vue'
 
 export default {
     setup() {
+        const message = ref('');
+
+        const login = () => {
+            message.value = 'Username or password is incorrect';
+        };
+
+        return { message, login }
+    },
+    methods: {
 
     },
     template: String.raw`
@@ -11,30 +20,26 @@ export default {
                     <v-col align-self="center" sm="6" md="4" lg="4" offset-sm="3" offset-md="4" offset-lg="4">
                         <v-card class="elevation-12">
                             <v-toolbar color="primary">
-                                <v-toolbar-title>Аутентификация</v-toolbar-title>
+                                <v-toolbar-title>Authentication</v-toolbar-title>
                             </v-toolbar>
                             <v-card-text>
                                 <v-form class="pb-0">
                                     <v-text-field prepend-icon="mdi-account"
-                                        name="login"
-                                        label="Логин"
+                                        name="username"
+                                        label="Username"
                                         type="text"
-
-                                        
                                         required
                                     ></v-text-field>
                                     <v-text-field prepend-icon="mdi-lock"
                                         name="password"
-                                        label="Пароль"
+                                        label="Password"
                                         id="password"
-
                                         type="password"
-                                        
                                         required
                                     ></v-text-field>
                                     <v-row class="ma-0">
-                                        <v-spacer class="text-red-darken-2 font-weight-medium pl-10 pt-1">{{ messsage }}</v-spacer>
-                                        <v-btn color="primary" @click="login"  min-width="120">Вход</v-btn>
+                                        <v-spacer class="text-red-darken-2 font-weight-medium pl-10 pt-1">{{ message }}</v-spacer>
+                                        <v-btn color="primary" @click="login"  min-width="120">Login</v-btn>
                                     </v-row>
                                 </v-form>
                             </v-card-text>
